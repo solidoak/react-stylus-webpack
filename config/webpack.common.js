0ce860ea749207303f6b52f2990670a6bc84b476
Oak Kim <solidoak.kim@gmail.com>
2016-11-28 16:42:42 -0500

    add eslint

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,19 +3,19 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 module.exports = {
   entry: {
-    app: path.join( __dirname, '../app' ),
-    vendor: path.join( __dirname, '../vendor.js' )
+    app: path.join(__dirname, '../app'),
+    vendor: path.join(__dirname, '../vendor.js')
   },
 
   output: {
     filename: '[name].js',
-    path: path.join( __dirname, '../dist' )
+    path: path.join(__dirname, '../dist')
   },
 
   plugins: [
-    new HtmlWebpackPlugin( {
-      template: path.join( __dirname, '../index.html')
-    } )
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '../index.html')
+    })
   ],
 
   resolve: {
