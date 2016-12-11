fcdc16f93a6a35e0194b1ea494fe74b897116a91
Oak Kim <solidoak.kim@gmail.com>
2016-12-11 17:12:18 -0500

    main app component

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './components/App'
+
 ReactDOM.render(
-  <p>Ready...</p>,
+  <App />,
   document.getElementById('app-container')
 )
